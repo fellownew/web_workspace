eddd110a6c5e8e261cf94b85d3f60c7a14d75aad
fellownew <hotttnew@naver.com>
2015-04-17 17:44:09 +0900

    javaScript - jQuery

--- BankSystem_FrontController/src/accountSQL.sql
+++ BankSystem_FrontController/src/accountSQL.sql
@@ -7,14 +7,18 @@ create table account(
 	account_id varchar(30) primary key,
 	account_password varchar(50) not null,
 	amount number(10) not null,
-	account_type varchar(50) foreign key references account_type(type_name),
-	own_id varchar(30) foreign key references customer(customer_id)
+	account_type varchar(50), 
+	own_id varchar(30),
+	foreign key(account_type) references account_type(type_name),
+	foreign key(own_id) references customer(customer_id)
 );
 
 
 create table account_list(
-	customer_id varchar(30) constraint ac_cus_id foreign key customer_id references customer, 
-	account_id varchar(30) constraint ac_ac_id foreign key account_id references account
+	customer_id varchar(30),
+	account_id varchar(30),
+	foreign key (customer_id) references customer(customer_id), 
+	foreign key (account_id) references account(account_id)
 )
 
 select *from customer
